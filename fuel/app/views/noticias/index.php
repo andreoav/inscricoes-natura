64f45dc2dd17a3928d5fa88a3c4c473c4eb3d041
Andreo Vieira <andreoav@gmail.com>
2012-06-22 16:31:05 -0300

    changes

--- fuel/app/views/noticias/index.php
+++ fuel/app/views/noticias/index.php
@@ -14,27 +14,12 @@
                             <div class="span1">
                                 <span class="label label-info"><?php echo Date::forge($noticia['created_at'])->format('%d/%m %H:%M'); ?></span>
                             </div>
-                            <div class="span8">
+                            <div class="span11">
                                 <p class="lead">Titulo</p>
                                 <p>Conteudo</p>
                             </div>
                         </div><hr />
                     <?php endforeach; ?>
-                </div>
-                <div class="span3">
-                    <div class="well" style="padding: 8px 0;">
-                        <ul class="nav nav-list">
-                            <li class="nav-header">Mais Lidas</li>
-                            <li class="active"><a href="#"><i class="icon-white icon-home"></i> Home</a></li>
-                            <li><a href="#"><i class="icon-book"></i> Library</a></li>
-                            <li><a href="#"><i class="icon-pencil"></i> Applications</a></li>
-                            <li class="nav-header">Another list header</li>
-                            <li><a href="#"><i class="icon-user"></i> Profile</a></li>
-                            <li><a href="#"><i class="icon-cog"></i> Settings</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#"><i class="icon-flag"></i> Help</a></li>
-                        </ul>
-                    </div>
                 </div>
             </div>
             <div class="row" id="loadMoreContainer">
