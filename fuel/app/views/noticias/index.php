2546c623b0f324eccfaefa8410cdceff9d4a30b7
Andreo Vieira <andreoav@gmail.com>
2012-06-22 16:13:33 -0300

    tests--

--- fuel/app/views/noticias/index.php
+++ fuel/app/views/noticias/index.php
@@ -26,7 +26,6 @@
                         <ul class="nav nav-list">
                             <li class="nav-header">Mais Lidas</li>
                             <li class="active"><a href="#"><i class="icon-white icon-home"></i> Home</a></li>
-
                             <li><a href="#"><i class="icon-book"></i> Library</a></li>
                             <li><a href="#"><i class="icon-pencil"></i> Applications</a></li>
                             <li class="nav-header">Another list header</li>
